55a1e5e72caf11760ebd0b03c11c9ab1abfed38d
Drew Johnson <112351892+drewjohnsn@users.noreply.github.com>
2023-06-30 16:24:24 -0400

    Added an incomplete disclaimer on the sentinelone-shellscript-installer.sh

---
+++ sentinelone-shellscript-installer.sh
@@ -0,0 +1,25 @@
+# Put your SentinelOne Token here
+sentinelToken = "YOUR S1 TOKEN"
+
+#You will need to upload the pkg to another site in order for the curl function to work.
+downloadLink = "YOUR DOWNLOAD LINK"
+
+pkgName = "YOUR PKG NAME.pkg"
+
+#########
+
+# Check if SentinelOne is already install
+if [ -d /Applications/SentinelOne] ;
+then
+    echo "SentinelOne is Already Installed"
+    exit 0
+else
+
+curl -L -o $pkgName $downloadLink
+
+#Pass the S1 Token to the PKG
+echo $sentinelToken /tmp/com.sentinelone.registration-token
+
+#Install Agent
+/usr/sbin/installer -pkg /tmp/$pkgName -target /
+fi
