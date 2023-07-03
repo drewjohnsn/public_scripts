c9a42e4ac1b88e2026289463bde7497461d7d4ba
Drew Johnson <112351892+drewjohnsn@users.noreply.github.com>
2023-07-03 15:40:29 -0400

    Add  variable that checks for DNS Filter in HKLM registry path. Ouput an RMM msg if it is. Create an if/else statement that calls the  variable and if it is installed, exit, if not, install it.

--- dns-filter-install-script.ps1
+++
@@ -1,5 +0,0 @@
-$NKEY = NInja-Property-Docs-Get 'Software' DNSFILTER productKey
-
-mkdir C:\temp
-Invoke-WebRequest -Uri "https://download.dnsfilter.com/User_Agent/Windows/DNS_Agent_Setup.msi" -OutFile "C:\temp\DNS_Agent_Setup.msi"
-msiexec /qn /i "C:\temp\DNS_Agent_Setup.msi" NKEY=$NKEY
